f6fc35e8986114ed6d7f637c48170f3a7f57bb9f
Kapil <kk7094948@gmail.com>
2026-05-24 18:36:10 +0530

    Conditional Statement

---
+++ 03_Conditional_Statement/Conditional_Statement.java
@@ -0,0 +1,26 @@
+// package 03_Conditional_Statement;
+
+public class Conditional_Statement {
+
+    public static void main(String [] args){
+        int x = 20 ;
+
+        if(x>10 && x<=20){
+            System.out.println("Hello") ;
+        }
+        else{
+            System.out.println("Bye") ;
+        }
+
+        int num1 = 5 ;
+        int num2 = 7 ;
+
+        if(num1 > num2){
+            System.out.println(num1) ;
+        }
+        else{
+            System.out.println(num2) ;
+         }
+    }
+    
+}
