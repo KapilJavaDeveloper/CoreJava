1858c9ad883f8cec0c1d7e51d205c3920430f837
Kapil <kk7094948@gmail.com>
2026-05-24 16:10:22 +0530

    Adding Relational Operator Program

---
+++ 01_Relational_Operator/Relational_Operators.java
@@ -0,0 +1,27 @@
+// package 01;
+
+public class Relational_Operators {
+
+ 
+    public static void main(String [] args){
+        int x = 5 ;
+        int y = 6 ;
+
+        boolean result1 = x<y ;
+        System.out.println(result1) ;
+
+        boolean result2 = x==y ;
+        System.out.println(result2) ;
+
+        double m = 8.8 ;
+        double n = 9.8 ;
+
+        boolean result3 = m<=n ;
+        System.out.println(result3) ;
+
+
+
+    }
+
+    
+}
