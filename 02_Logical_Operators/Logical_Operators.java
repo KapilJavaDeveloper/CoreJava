4abe78c575e224899adb1c42f1ebb39c16dbf885
Kapil <kk7094948@gmail.com>
2026-05-24 17:21:45 +0530

    Logical Operators in Java

---
+++ 02_Logical_Operators/Logical_Operators.java
@@ -0,0 +1,30 @@
+// package 02_Logical_Operators;
+
+public class Logical_Operators {
+    public static void main(String [] args){
+        System.out.println("Logical Operators") ;
+
+        int x = 7 ;
+        int y = 5 ;
+        int a = 5 ;
+        int b = 9 ;
+
+        boolean result1 = x>y && a>b ;
+        System.out.println(result1) ;
+
+        boolean result2 = x>y || a>b ;
+        System.out.println(result2) ;
+
+        boolean result3 = !result2 ;
+        System.out.println(result3) ; 
+
+
+
+
+
+
+
+
+    }
+    
+}
