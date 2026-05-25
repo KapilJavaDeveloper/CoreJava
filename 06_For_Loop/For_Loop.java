faf45236cdd5367c73bdac2509af0bbc6ac25e6a
Kapil <kk7094948@gmail.com>
2026-05-25 08:42:21 +0530

    For Loop

---
+++ 06_For_Loop/For_Loop.java
@@ -0,0 +1,14 @@
+// package 06_For_Loop;
+
+public class For_Loop {
+    public static void main(String [] args){
+
+        String name = "Isskander"  ;
+
+        for(int i=0 ; i<10 ;i++){
+            System.out.println(name) ;
+        }
+    }
+
+    
+}
