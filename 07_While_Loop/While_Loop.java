612b80781c02ac534a9a8e83de1f8450462831cb
Kapil <kk7094948@gmail.com>
2026-05-25 09:05:50 +0530

    While Loop

---
+++ 07_While_Loop/While_Loop.java
@@ -0,0 +1,16 @@
+// package 07_While_Loop;
+
+public class While_Loop {
+    public static void main(String [] args){
+
+        int i = 1 ;
+        String name = "Isskander"  ;
+
+        while(i<5){
+            System.out.println(name) ;
+            i++ ;
+        }
+
+    }
+    
+}
