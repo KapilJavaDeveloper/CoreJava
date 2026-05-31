fe483f453718146dcd0e616e2e62bf0bc5483db0
Kapil <kk7094948@gmail.com>
2026-05-31 07:55:58 +0530

    Static Block

---
+++ 19_Static_Block/Static_Block.java
@@ -0,0 +1,32 @@
+// package 19_Static_Block;
+
+class Student{
+    static String name = "Isskander"  ;
+    
+    static{
+        System.out.println("Name be: "  + name) ;
+    }
+
+    public Student(){
+        System.out.println("Default Constructor of Student class") ;
+    }
+
+    int id = 12 ;
+    
+    {
+        System.out.println("Id of Student be: " + id) ;
+    }
+    
+}
+
+public class Static_Block {
+    public static void main(String [] args) throws Exception{
+
+        Class.forName("Student") ;
+
+        Student obj = new Student() ;
+
+
+    }
+    
+}
