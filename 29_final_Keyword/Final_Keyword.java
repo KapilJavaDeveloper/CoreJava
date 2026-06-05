8eaa848844eab84f016abb2e5f33359553f77faf
Kapil <kk7094948@gmail.com>
2026-06-05 06:29:46 +0530

    final keyword in Java

---
+++ 29_final_Keyword/Final_Keyword.java
@@ -0,0 +1,16 @@
+public final class Final_Keyword {
+
+    final void display(){
+        System.out.println("display method.") ;
+
+    }
+    public static void main(String [] args){
+        System.out.println("final Keyword in Java") ;
+
+        final int num = 89 ;
+
+        final Final_Keyword obj = new Final_Keyword() ;
+
+    }
+    
+}
