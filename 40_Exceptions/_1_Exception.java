9869b896cdb7501168384cfbbab50324e402c4e3
Kapil <kk7094948@gmail.com>
2026-06-09 08:14:51 +0530

    Exception in Java

---
+++ 40_Exceptions/_1_Exception.java
@@ -0,0 +1,11 @@
+public class _1_Exception {
+    public static void main(String [] args){
+        int a = 10 ;
+        int b = 0; 
+
+        int divide = a/b ;
+
+        System.out.println(divide) ;
+    }
+    
+}
