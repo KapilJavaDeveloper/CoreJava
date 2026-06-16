cbb90f7e1a41f79ab1943eae5221876b225e1dfc
Kapil <kk7094948@gmail.com>
2026-06-16 07:06:33 +0530

    TreeSet implements Set

---
+++ 50_Set_Collection/_2_TreeSet.java
@@ -0,0 +1,21 @@
+// package 50_Set_Collection;
+
+import java.util.Set ;
+import java.util.TreeSet ;
+public class _2_TreeSet {
+    public static void main(String [] args){
+        System.out.println("TreeSet Program.");  
+
+        Set<Integer> set = new TreeSet<>() ;
+
+        set.add(62) ;
+        set.add(54) ;
+        set.add(82) ;
+        set.add(21) ;
+
+        System.out.println("set be: " + set) ;
+
+
+    }
+    
+}
