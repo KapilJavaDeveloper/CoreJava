c811dcde3947a9bf60af813efe7c0d7bd48783d8
Kapil <kk7094948@gmail.com>
2026-06-18 07:00:05 +0530

    1 for Each Loop

---
+++ 54_forEach_Loop/_1_forEach_Loop.java
@@ -0,0 +1,22 @@
+// package 54_forEach_Loop;
+import java.util.function.Consumer ;
+import java.util.List ;
+import java.util.Arrays;
+public class _1_forEach_Loop {
+    public static void main(String [] args){
+        System.out.println("for Each Loop Program") ;
+
+        Consumer<Integer> con = new Consumer<>(){
+            public void accept(Integer n){
+                System.out.print(n + " ") ;
+            }
+        } ;
+
+        List<Integer> list = Arrays.asList(6,3,9,1) ;
+
+        list.forEach(con) ;
+
+
+    }
+    
+}
